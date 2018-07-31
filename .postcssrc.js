fb392ae48006f360076f35bd2ac1173eed2f1170
shen-zhao <616510680@qq.com>
2018-07-31 14:34:45 +0800

    feat: add validate

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,6 +3,15 @@
 module.exports = {
   "plugins": {
     "postcss-import": {},
-    "postcss-url": {}
+    "postcss-url": {},
+    "autoprefixer": {
+      browsers: [
+        "> 1%",
+        "last 2 versions",
+        "Firefox >= 3.6",
+        "Opera 12.1",
+        "ie 8"
+      ]
+    }
   }
 }
