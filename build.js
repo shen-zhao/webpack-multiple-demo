6801d9299a6c3057a0fd1291d317d7f3feacc93b
shen-zhao <616510680@qq.com>
2018-08-05 22:04:00 +0800

    feat:修改commons引入规则

--- build.js
+++ build.js
@@ -1,30 +1,28 @@
 /* 
-    template: 页面入口名称, 位置src/pages,
-    main: js入口文件路径, 🌰 src/js/ + js名称, 支持子文件夹例如index/index.js会自动生成index目录,
-    commons: 插件来源为node_modules和入口脚本的本地依赖文件, 包括jQuery, art-template等,
+    *template: 页面入口名称, 位置src/pages,
+    *main: js入口文件路径, 🌰 src/js/ + js名称, 支持子文件夹例如index/index.js会自动生成index目录,
+    commons: 插件来源为node_modules和入口脚本的本地依赖文件, 包括jQuery, art-template等, 默认引入, false不引入
     echarts: echarts库, 注: echarts库比较大, 没有打入vendors, 后续如有大型插件需要单独引用的需修改配置,
-    stylesheet: 所有公共样式，如common.scss/form.scss/table.scss/reset.scss等, 为false时不引入
+    stylesheet: 所有公共样式，如common.scss/form.scss/table.scss/reset.scss等, 默认引入, 为false时不引入
  */
 module.exports = [
     {
         template: 'index',
-        main: 'index.js',
-        commons: true
+        main: 'index.js'
     },
     {
         template: 'detail',
         main: 'detail.js',
-        commons: true,
         echarts: true
     },
     {
         template: 'list',
         main: 'list.js',
-        commons: true
     },
     {
         template: '404',
         main: '404.js',
+        commons: false,
         stylesheet: false
     }
 ];
